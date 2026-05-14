c2a530959b033387918b982b8a7950710e7bd5b7
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-05-15 05:54:16 +0700

    Remove countdown and exit procedure from setup script
    Removed countdown timer and exit message for Termux.

--- setup-small.sh
+++ setup-small.sh
@@ -160,15 +160,5 @@ echo "video.sh   → Download videos 🎬"
 echo "song.sh    → Download audio 🎵"
 echo "ftp.sh     → Start FTP server 📡"
 echo "mv.sh     → Download video and song one time"
-echo " ----------Exit 10s wait close---------- "
+echo " ----------Exit CRTL+C---------- "
 
-for i in 10 9 8 7 6 5 4 3 2 1
-do
-    echo "$i"
-    sleep 1
-done
-
-echo "-----------👋 Bye-----------"
-
-# Close Termux app
-pkill -f com.termux
