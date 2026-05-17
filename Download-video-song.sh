1131a16d35a075faa9d004fdb1a87516f6619b95
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-05-17 19:37:27 +0700

    Rename video-quality.sh to video.sh and update script

--- Download-video-song.sh
+++ Download-video-song.sh
@@ -24,50 +24,13 @@ grep -qxF 'set -U fish_user_paths $fish_user_paths $HOME' ~/.config/fish/config.
 echo 'set -U fish_user_paths $fish_user_paths $HOME' >> ~/.config/fish/config.fish
 
 # ---------------- VIDEO SCRIPT ----------------
-cat > ~/video-quality.sh << 'EOF'
+cat > ~/video.sh << 'EOF'
 #!/data/data/com.termux/files/usr/bin/bash
 
 DOWNLOAD_FOLDER="/storage/emulated/0/Movies"
 mkdir -p "$DOWNLOAD_FOLDER"
 
 echo "🎬 Multi Video Downloader (CTRL+C to exit)"
-echo ""
-
-echo "=============================="
-echo " Select Video Quality"
-echo "=============================="
-echo "1. 144p"
-echo "2. 240p"
-echo "3. 360p"
-echo "4. 480p"
-echo "5. 720p"
-echo "6. 1080p"
-echo "7. 1440p"
-echo "8. 2160p (4K)"
-echo "9. 4320p (8K)"
-echo "=============================="
-
-read -p "Choose quality (1-9): " choice
-
-case $choice in
-    1) QUALITY=144 ;;
-    2) QUALITY=240 ;;
-    3) QUALITY=360 ;;
-    4) QUALITY=480 ;;
-    5) QUALITY=720 ;;
-    6) QUALITY=1080 ;;
-    7) QUALITY=1440 ;;
-    8) QUALITY=2160 ;;
-    9) QUALITY=4320 ;;
-    *)
-        echo "❌ Invalid choice"
-        exit
-        ;;
-esac
-
-echo ""
-echo "✅ Selected ${QUALITY}p"
-echo ""
 
 while true; do
     read -p "Enter video URL: " url
@@ -77,23 +40,20 @@ while true; do
         continue
     fi
 
-    echo ""
-    echo "⬇️ Downloading ${QUALITY}p ..."
-    echo "🔗 $url"
+    echo "⬇️ Downloading $url ..."
 
     yt-dlp \
-        -f "bestvideo[height<=${QUALITY}][ext=mp4][fps<=60]+bestaudio[ext=m4a]/best[height<=${QUALITY}]" \
+        -f "bestvideo[height<=720][ext=mp4][fps<=60]+bestaudio[ext=m4a]/mp4" \
         --merge-output-format mp4 \
         -S "vcodec:h264,lang,quality,res,fps,hdr:12,acodec:aac" \
         -o "$DOWNLOAD_FOLDER/%(title)s.%(ext)s" \
         "$url" &
 
-    echo "✅ Download started!"
-    echo ""
+    echo "✅ Started!"
 done
 EOF
 
-chmod +x ~/video-quality.sh
+chmod +x ~/video.sh
 
 # ---------------- AUDIO SCRIPT ----------------
 cat > ~/song.sh << 'EOF'
