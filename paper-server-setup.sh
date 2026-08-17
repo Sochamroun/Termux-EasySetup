b7f3518fc645fe3668449f914f65c8f459790936
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-08-18 06:29:16 +0700

    Add support for multiple OpenJDK versions in setup

--- paper-server-setup.sh
+++ paper-server-setup.sh
@@ -10,7 +10,7 @@ sleep 5
 
 echo "Install required packages🍗"
 
-yes | pkg install curl iproute2 wget jq openjdk-21 -y
+yes | pkg install curl iproute2 wget jq openjdk-21 openjdk-17 openjdk-25 -y
 
 echo ""
 echo "Enter server folder 📂 name:"
@@ -191,11 +191,11 @@ echo "===================================="
 
 echo ""
 echo "📁 Folder:"
-echo "cd ~/$SERVERNAME"
+echo "cd $SERVERNAME"
 
 echo ""
 echo "▶ Start server:"
-echo "bash ~/$SERVERNAME.sh"
+echo "bash $SERVERNAME.sh"
 
 echo ""
 echo "🔓 Server Enabled 🧐"
