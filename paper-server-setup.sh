cab618845e7fad5d0a466b2c2354e2f1f7d5652c
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-09-06 19:22:28 +0700

    Update paper-server-setup.sh

--- paper-server-setup.sh
+++ paper-server-setup.sh
@@ -151,7 +151,9 @@ server-port=$SERVER_PORT
 max-players=$MAX_PLAYERS
 hardcore=$HARDCORE
 motd=$MOTD
-network-compression-threshold=64
+network-compression-threshold=256
+sync-chunk-writes=false
+pause-when-empty-seconds=60
 EOF
 
 # Add level-seed only if user entered one
@@ -201,7 +203,7 @@ echo ""
 echo "🔓 Server Enabled 🧐"
 
 echo "Wait Check"
-sleep 5
+sleep 3
 
 echo "true = Premium ✅"
 echo "false = Offline/Cracked 🤫" 
